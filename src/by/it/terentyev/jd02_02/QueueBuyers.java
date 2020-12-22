4b1c8b3154a15783d93e094136ee8649fc960a65
Teren <byvladimirt@gmail.com>
2020-12-22 23:24:01 +0300

    JD02_01 Task B

---
+++ src/by/it/terentyev/jd02_02/QueueBuyers.java
@@ -0,0 +1,16 @@
+package by.it.terentyev.jd02_02;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class QueueBuyers {
+    private static final Deque<Buyer> deque = new ArrayDeque<>();
+
+    static synchronized void add(Buyer buyer){
+        deque.addLast(buyer);
+    }
+
+    static synchronized Buyer extract (){
+        return deque.pollFirst();
+    }
+}
