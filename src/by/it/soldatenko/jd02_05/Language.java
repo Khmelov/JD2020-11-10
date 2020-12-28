36fb063a9ecf2b615e7abba151fb90facd32728b
ivansoldatenko <ivansoldatenko80@gmail.com>
2020-12-28 17:16:18 +0300

    hw 0206

--- src/by/it/soldatenko/jd02_05/Language.java
+++ src/by/it/soldatenko/jd02_05/Language.java
@@ -7,12 +7,9 @@ public enum Language {
 
     ISTANCE;
     ResourceBundle bundle;
-//    Language(){
-//        setLocale(Locale.getDefault());
-//    }
+
    final void setLocale(Locale locale){
 
-//       String BASE = "by.it.soldatenko.jd02_05.resources.language";
        String BASE= Language.class.getName().replace(Language.class.getSimpleName(), "")+"resources.language";
        bundle=ResourceBundle.getBundle(BASE, locale);
     }
