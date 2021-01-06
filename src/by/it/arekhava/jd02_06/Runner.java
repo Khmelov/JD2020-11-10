dde6c24e3a11b92def2115c7241a77831ab5178d
N <no5110059@gmail.com>
2021-01-06 18:42:22 +0300

    test

---
+++ src/by/it/arekhava/jd02_06/Runner.java
@@ -0,0 +1,12 @@
+package by.it.arekhava.jd02_06;
+
+public class Runner {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                Logger.getLogger().log("Test");
+            }).start();
+
+        }
+    }
+}
