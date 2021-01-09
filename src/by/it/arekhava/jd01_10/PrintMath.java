828ce0ecaf01aca520c995f810d8340fa9436589
N <no5110059@gmail.com>
2021-01-09 15:39:58 +0300

    test

--- src/by/it/arekhava/jd01_10/PrintMath.java
+++ src/by/it/arekhava/jd01_10/PrintMath.java
@@ -1,37 +1,44 @@
 package by.it.arekhava.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
 import java.sql.SQLOutput;
 
 public class PrintMath {
-    public static void main(String[] args) {
-        Class<?> aClass = Math.class;
-        Method[] methods = aClass.getDeclaredMethods();
-        for (Method method : methods){
-            StringBuilder out=new StringBuilder();
 
-            int modifiers=method.getModifiers();
-            if (Modifier.isPublic(modifiers)) {
-                out.append("public ");
+        public static void main(String[] args) {
+            Class<Math> aClass = Math.class;
+            Method[] methods = aClass.getDeclaredMethods();
+            Field[] fields = aClass.getDeclaredFields();
+            for (Field field : fields) {
+                if ((field.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC){
+                    String contract = "public static " +
+                            field.getType() + " " + field.getName() + "(" +
+                            ")";
+                    System.out.println(contract);
+                }
             }
-            if (Modifier.isStatic(modifiers)) {
-                out.append("static ");
-            }
-            Class<?> returnType = method.getReturnType();
-            String nameRetuType = returnType.getSimpleName();
-            out.append(nameRetuType).append(" ");
-            out.append(method.getName()).append("(");
-            Parameter[] parameters = method.getParameters();
-            String delimiter=",";
-            for (Parameter parameter:parameters) {
-                out.append(delimiter).append(parameter.getType().getSimpleName());
-            }
-              out.append(")");
-            System.out.println(out);
+
+            for (Method method : methods) {
+                if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC){
+                    StringBuilder contract = new StringBuilder();
+                    contract.append("public static ");
+                    Class<?> returnType = method.getReturnType();
+                    String nameReturnType = returnType.getSimpleName();
+                    contract.append(nameReturnType).append(" ").append(method.getName()).append("(");
+                    Parameter[] parameters = method.getParameters();
+                    String delimiter = "";
+                    for (Parameter parameter : parameters) {
+                        contract.append(delimiter).append(parameter.getType().getSimpleName());
+                        delimiter = ",";
+                    }
+                    contract.append(")");
+                    System.out.println(contract);
+                }
             }
 
         }
+    }
 
-}
